8728b4a21615b0b10343f4e54ac5ff6c1235f438
Quyoshbek <xudoykulov97@gmail.com>
2022-11-24 14:59:58 +0500

    first page

--- lib/main.dart
+++ lib/main.dart
@@ -39,6 +39,7 @@ class MyHomePage extends StatelessWidget {
       ),
       appBar: AppBar(),
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           CarouselSlider.builder(
             itemCount: 3,
@@ -50,7 +51,7 @@ class MyHomePage extends StatelessWidget {
                   child: Text(itemIndex.toString()),
             ),
             options: CarouselOptions(
-              height: 250,
+              height: 200,
               aspectRatio: 2.0,
               viewportFraction: 0.8,
               initialPage: 0,
@@ -63,6 +64,74 @@ class MyHomePage extends StatelessWidget {
               autoPlayCurve: Curves.fastOutSlowIn,
             ),
           ),
+          Container(
+            padding: EdgeInsets.only(top: 30, left: 20, bottom: 30),
+            child: Text("Progress", style: TextStyle(fontSize: 32),),
+          ),
+          Expanded(child: ListView.builder(
+              itemCount: 8,
+              scrollDirection: Axis.vertical,
+              itemBuilder: (context, index) {
+                return Container(
+                  margin: EdgeInsets.only(
+                      left: 30, right: 30, top: 10, bottom: 10),
+                  padding: EdgeInsets.all(20),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Colors.white,
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Container(
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            Container(
+                              padding:EdgeInsets.all(20),
+                              margin: EdgeInsets.only(right: 20),
+                              decoration: BoxDecoration(
+                                color: Color(0xff7837F5),
+                                borderRadius: BorderRadius.circular(5),
+                              ),
+                              child: Icon(Icons.event_note_outlined, color: Colors.white,),
+                            ),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              children: [
+                                Text("Design Changes", style: TextStyle(color: Colors.black, fontSize: 18,fontWeight: FontWeight.bold),),
+                                Text("2 days ago", style: TextStyle(color: Colors.grey, fontSize: 14),)
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                      PopupMenuButton(
+                        onSelected: (value) {
+                          // your logic
+                        },
+                        itemBuilder: (BuildContext bc) {
+                          return const [
+                            PopupMenuItem(
+                              child: Text("Add"),
+                              value: '/add',
+                            ),
+                            PopupMenuItem(
+                              child: Text("Remove"),
+                              value: '/remove',
+                            ),
+                            PopupMenuItem(
+                              child: Text("Update"),
+                              value: '/update',
+                            )
+                          ];
+                        },
+                      )
+                    ],
+                  ),
+                );
+              }),),
         ],
       ),
     );
